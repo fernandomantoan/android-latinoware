38e0fac7dfa204a8d2f033b935c3f5b12f0f9473
Fernando Mantoan <contato@fernandomantoan.com>
2015-10-07 10:52:19 -0300

    Removing deprecated Apache HTTP Client

--- app/src/main/java/com/fernandomantoan/latinoware/support/WebClient.java
+++ app/src/main/java/com/fernandomantoan/latinoware/support/WebClient.java
@@ -1,43 +1,64 @@
 package com.fernandomantoan.latinoware.support;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class WebClient {
-	
-	private DefaultHttpClient client;
-	private HttpGet get;
-	private HttpResponse response;
-	
+	private String mUrl;
+
 	public WebClient(String url) {
-		client = new DefaultHttpClient();
-		get = new HttpGet(url);
+		mUrl = url;
 	}
-	
+
 	public String get() {
-		
 		String result = null;
-		
+		InputStream is = null;
 		try {
-	    	get.setHeader("Accept", "application/json");
-	    	get.setHeader("Content-type", "application/json");
-	    	
-	    	response = client.execute(get);
-	    	if(response.getStatusLine().getStatusCode() != 200) {
-	    		throw new RuntimeException("StatusCode diferente de 200");
-	    	}
-	    	result = EntityUtils.toString(response.getEntity());
-	    	
+			URL url = new URL(mUrl);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Accept", "application/json");
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setConnectTimeout(5000);
+			connection.connect();
+			int responseCode = connection.getResponseCode();
+
+			if (responseCode != HttpURLConnection.HTTP_OK) {
+				throw new RuntimeException("StatusCode diferente de 200");
+			}
+
+			is = connection.getInputStream();
+
+			result = readResponse(is);
 		} catch (Exception e) {
 			String message = "Não foi possível conectar com o servidor";
-			Log.e("APP002", message, e);
+			Log.e("WEBCLIENT", message, e);
 			throw new RuntimeException(message, e);
 		}
-		
+		return result;
+	}
+
+	protected String readResponse(InputStream is) throws IOException {
+		final Reader isReader = new InputStreamReader(is, "UTF-8");
+		final BufferedReader reader = new BufferedReader(isReader);
+
+		String result = "";
+		String line = "";
+
+		while ((line = reader.readLine()) != null) {
+			result += line;
+		}
+		reader.close();
+		isReader.close();
+		is.close();
+
 		return result;
 	}
 }
